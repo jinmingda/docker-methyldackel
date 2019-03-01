5468182f5f1590d8f945bbf0ed00962c8e924248
Mingda Jin <jinmingda77@gmail.com>
2019-02-28 21:49:48 -0800

    Revert "Intentionally add some bad code in the applet"
    This reverts commit 6f76311c06750d24906455608df08a4febb54568.

--- dnanexus/methyldackel/src/script.sh
+++ dnanexus/methyldackel/src/script.sh
@@ -5,8 +5,6 @@ main() {
   # set -x to print trace
   set -e -x
 
-  somenonsense
-
   # login to pull images from ECR
   eval $(docker run --rm -e AWS_ACCESS_KEY_ID="$aws_access_key_id" -e AWS_SECRET_ACCESS_KEY="$aws_secret_access_key" mesosphere/aws-cli ecr get-login --no-include-email --region us-east-1)
 
@@ -41,7 +39,7 @@ main() {
     opts="$opts $extra_options"
   fi
 
-  docker running -v /data/:/tmp 002226384833.dkr.ecr.us-east-1.amazonaws.com/methyldackel:0.3.0 extract reference.fa ${bamfile_name} ${opts}
+  docker run -v /data/:/tmp 002226384833.dkr.ecr.us-east-1.amazonaws.com/methyldackel:0.3.0 extract reference.fa ${bamfile_name} ${opts}
 
   bedfile_link=`dx upload /data/${bedfile_name} --brief`
   dx-jobutil-add-output bedfile_link "$bedfile_link"
