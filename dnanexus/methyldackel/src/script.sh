21d022aeb3b6ed3c2d82b94f2e42fea1a4daa3ef
Mingda Jin <jinmingda@users.noreply.github.com>
2019-02-28 16:03:41 -0800

    Allow applet to pull from private ECR (#8)

--- dnanexus/methyldackel/src/script.sh
+++ dnanexus/methyldackel/src/script.sh
@@ -5,6 +5,15 @@ main() {
   # set -x to print trace
   set -e -x
 
+  # install awscli from local (only works on ubuntu 16.04)
+  python3 /tmp/awscli-bundle/install -b ~/bin/aws
+  # test awscli
+  aws --version
+  # login to pull images from ECR
+  export AWS_ACCESS_KEY_ID="$aws_access_key_id"
+  export AWS_SECRET_ACCESS_KEY="$aws_secret_access_key"
+  eval $(aws ecr get-login --no-include-email --region us-east-1)
+
   opts=""
 
   mkdir -p /data/
@@ -36,7 +45,7 @@ main() {
     opts="$opts $extra_options"
   fi
 
-  docker run -v /data/:/tmp methyldackel extract reference.fa ${bamfile_name} ${opts}
+  docker run -v /data/:/tmp 002226384833.dkr.ecr.us-east-1.amazonaws.com/methyldackel:0.3.0 extract reference.fa ${bamfile_name} ${opts}
 
   bedfile_link=`dx upload /data/${bedfile_name} --brief`
   dx-jobutil-add-output bedfile_link "$bedfile_link"
