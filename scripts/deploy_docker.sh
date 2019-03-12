4e6058c5edcd7ea08a09f6f79e47e1201db9d709
Mingda Jin <jinmingda@users.noreply.github.com>
2019-03-12 00:35:07 -0700

    Add cache src for docker build (#17)

--- scripts/deploy_docker.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-set -e -x
-
-pip install awscli
-eval $(aws ecr get-login --no-include-email --region us-east-1)
-docker push $ECR/$IMAGE:$VERSION_CONTAINER
-docker push $ECR/$IMAGE:latest
-
